88bba951afe799e30d6a090cd68b1a75a0dc6cba
Mickael Daniel <daniel.mickael@gmail.com>
2018-09-04 18:43:55 +0200

    site update

--- about.md
+++ about.md
@@ -2,7 +2,6 @@
 layout: page
 title: About
 permalink: /about/
-redirect_from: "/"
 ---
 
 32 years old JavaScript dev based in Lyon, France.
--- search/search.js
+++ search/search.js
@@ -1,78 +1,89 @@
-const yo       = require('yo-yo');
-const qs       = require('qs');
-const bel      = require('bel');
-const path     = require('path');
+const yo = require('yo-yo');
+const qs = require('qs');
+const bel = require('bel');
+const path = require('path');
 const delegate = require('delegate');
-const throttle = require('throttle');
+const debounce = require('lodash.debounce');
 
 const BASE_URL = 'http://mklabs.github.io/';
 
 // Search def
 class Search {
-  get endpoint () {
+  get endpoint() {
     return 'https://api.github.com/search/code?q=';
   }
 
-  constructor (template, options = {}) {
+  constructor(template, options = {}) {
     this.template = template;
     this.options = options;
   }
 
-  search (query, options = {}) {
-    return this.fetch(query)
-      .then(this.dom.bind(this, query));
+  search(query, options = {}) {
+    // console.log('Searching with query', query);
+    if (!query) {
+      return Promise.resolve().then(() => this.template(query, {}));
+    }
+
+    return this.fetch(query).then(this.dom.bind(this, query));
   }
 
-  url (query, { language, repo }) {
+  url(query, { language, repo }) {
     return this.endpoint + `${query}+in:file+language:${language}+repo:${repo}`;
   }
 
-  fetch (query) {
+  fetch(query) {
     const opts = { ...this.options };
-    const optsArchivedRepo = { ...this.options, repo: this.options.archivedRepo}
 
-    return Promise.all([
-      fetch(this.url(query, opts)),
-      fetch(this.url(query, optsArchivedRepo))
-    ])
-    .catch(e => console.error('HTTP ERR', e))
-    .then(([res, resArchived]) => {
-      return Promise.all([res.json(), resArchived.json()]);
-    })
-    .then(this.fetchBody)
+    return fetch(this.url(query, opts))
+      .catch(e => console.error('HTTP ERR', e))
+      .then(this.fetchBody);
   }
 
-  fetchBody([res, resArchived]) {
-    console.log('=>', res);
-    console.log('=>', resArchived);
-    return res;
+  fetchBody(res) {
+    // console.log('=>', res);
+    return res.json();
   }
 
-  dom (query, res) {
+  dom(query, res) {
+    if (res.statusText == 'Forbidden') {
+      // console.log('Forbidden request', res);
+      return this.template(query, {
+        error: 'API rate limit exceeded. Try again later.'
+      });
+    }
+
     return this.template(query, res);
   }
 }
 
 // Search entry point
 const opns = (template, options = {}) => {
-  if (!template) throw new Error('Missing template');
-  if (typeof template !== 'function') throw new Error('Template must be a function');
+  if (!template) {
+    throw new Error('Missing template');
+  }
+
+  if (typeof template !== 'function') {
+    throw new Error('Template must be a function');
+  }
+
   return new Search(template, options);
 };
 
 // Templates
-const template = (query,  { items }) => {
+const template = (query, { error, items }) => {
+  if (error) return errorTemplate(query, error);
   if (!items || !items.length) return noResult(query);
 
   return bel`<section class="js-result">
-    ${items.map((item) => {
-      let name = path.basename(item.name)
+    ${items.map(item => {
+      let name = path.basename(item.name);
       name = name.replace(/\d{4}-\d\d?-\d\d?-/, '');
-      name = name.replace(/\.md/, '')
+      name = name.replace(/\.md/, '');
 
-      const parts = item.name.split('-')
-      let href = BASE_URL + parts.slice(0, 3).join('/') + '/' + parts.slice(3).join('-')
-      href = href.replace(/\.md/, '.html')
+      const parts = item.name.split('-');
+      let href = BASE_URL;
+      href += parts.slice(0, 3).join('/') + '/' + parts.slice(3).join('-');
+      href = href.replace(/\.md/, '.html');
 
       return bel`<div class="opns-item">
         <header class="header">
@@ -86,38 +97,75 @@ const template = (query,  { items }) => {
 };
 
 // No result template helper
-const noResult = (query) => {
-  return bel`<div class="opns-item">
-    <p>No results for ${query}</p>
-  </div>`;
+const noResult = query => {
+  // console.log('no result template with query "%s"', query);
+  return bel`
+  <section class="js-result">
+    <div class="opns-item">
+      <p>
+      ${query ? `No results for ${query}` : `Type in a query in the form below.`}
+      </p>
+    </div>
+  </section>
+  `;
+};
+
+const errorTemplate = (query, error) => {
+  return bel`
+  <section class="js-result">
+    <div class="opns-item">
+      <p class="error">
+      ${error ? `${error}` : `Got an error for query "${query}"`}
+      </p>
+    </div>
+  </section>
+  `;
 };
 
 // Init the view
 const view = opns(template, {
   repo: 'mklabs/mklabs.github.com',
-  archivedRepo: 'mklabs/blog.mklog.fr',
   language: 'markdown'
 });
 
 const input = document.querySelector('.js-search');
+const submit = document.querySelector('.js-submit');
 const container = document.querySelector('.js-results');
 
-delegate(document.body, '.js-search', 'input', (e) => {
-  const value = input.value;
-  search(value);
-}, false);
+delegate(
+  document.body,
+  '.js-search',
+  'input',
+  debounce(e => {
+    const value = input.value;
+    search(value);
+  }, 300),
+  false
+);
 
-let initialQuery = qs.parse(location.search.replace(/^\?/, '')).q;
-if (!initialQuery) {
-  initialQuery = 'ES6';
-}
+delegate(
+  document.body,
+  '.js-form',
+  'submit',
+  e => {
+    e.preventDefault();
 
-const search = (value) => {
-  return view.search(value)
-    .then((el) => {
-      yo.update(container, el)
-      input.focus();
-    });
+    const value = input.value;
+    search(value);
+  },
+  false
+);
+
+const initialQuery = qs.parse(location.search.replace(/^\?/, '')).q || '';
+const search = value => {
+  if (value) {
+    document.querySelector('.js-search').value = value;
+  }
+
+  return view.search(value).then(el => {
+    yo.update(container, el);
+    input.focus();
+  });
 };
 
 search(initialQuery);
