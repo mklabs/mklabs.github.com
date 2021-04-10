5c6965471aae59ad78f9d5de22f0d5403ed1570c
Mickael Daniel <daniel.mickael@gmail.com>
2021-04-10 12:42:11 +0200

    Remove netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,6 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-gatsby-cloud`,
-    `gatsby-plugin-netlify`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
