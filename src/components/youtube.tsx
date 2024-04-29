60f69c505a1dce5417aa38cfdfad83d31eff2edf
Mickael Daniel <daniel.mickael@gmail.com>
2024-04-29 19:46:38 +0200

    rm old content

--- src/components/youtube.tsx
+++
@@ -1,9 +0,0 @@
-import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
-
-const YouTube = (props) => {
-    return <LiteYouTubeEmbed {...props} />
-}
-
-export default YouTube
