a63bfe1e6c40e285671fe2a03f99d68dcf96367f
Mickael Daniel <daniel.mickael@gmail.com>
2025-04-01 19:13:57 +0200

    home and footer tweaks

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -21,6 +21,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+	{ /*
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -32,15 +33,66 @@ export default function Footer() {
             <p className="ml-2 h-7">rss</p>
           </a>
         </li>
+	*/
+	}
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
-            target="_blank"
+            href="https://www.fab.com/sellers/mklabs"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">fab</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
             href="https://github.com/mklabs"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/mickaeldaniel"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">linkedin</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            href="https://x.com/mklabs"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">twitter</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            href="https://www.youtube.com/@MickaelDaniel"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">youtube</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            href="https://discord.gg/d4rs4vcX6t"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">discord</p>
           </a>
         </li>
       </ul>
